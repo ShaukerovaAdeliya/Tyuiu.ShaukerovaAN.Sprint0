51f9182e35e861c9a32ed540562fffc671714144
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-09-11 19:48:29 +0500

    Task1

---
+++ Tyuiu.ShaukerovaAN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShaukerovaAN.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Adeliya";
+            int age = 17;
+            Console.WriteLine("Hello my name is " + name + " I'm " + age + " years old!");
+            Console.ReadKey(); 
+        }
+    }
+}
